aca737e774da619cd325a4bcc23d559540111dd2
GeMine1 <robingebert99@gmail.com>
2020-01-26 14:49:12 +0100

    Changes#1

---
+++ adb-tools/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adb_tools
+{
+    static public class Methods
+    {
+        public static string help()
+        {
+            string value = "";
+            value += "List of commands and short description\ntype <command> -h for detailed help\n\n";
+            value += "bluetooth     debug over bluetooth for wear";
+            value += "\nwifi          debug over wifi for android";
+            value += "\npull          get file to .\\files";
+            value += "\npush          send file from files";
+            value += "\n";
+
+            return value;
+        }
+
+        public static string wifi()
+        {
+            
+        }
+
+
+    }
+}
---
+++ adb-tools/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace adb_tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string return_value = "";
+            switch (args[0])
+            {
+                case "help":
+                    return_value = Methods.help();
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(return_value);
+        }
+    }
+}
